e39aa84a993cc43f94f7e3e3d621630f27de2085
Olegmaidaniuk <omaydanuk@gmail.com>
2022-07-22 10:27:35 +0300

    js first

---
+++ JS june 2022 Maidaniuk/First2022lesson1HMWRKMaidaniuk.js
@@ -0,0 +1,51 @@
+// - Створити змінні. Присвоїти кожному з них значення:
+// 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+//     Вивести кожну змінну за допомогою: console.log , alert, document.write
+
+let myr;
+myr='hello';
+console.log(myr);
+// alert(myr);
+document.write(myr);
+
+
+
+
+
+let tab;
+tab = 1;
+console.log(tab)
+// alert(tab);
+document.write(tab);
+
+
+
+let hi;
+hi=true;
+console.log(hi);
+
+
+
+
+
+//
+// - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ.
+// З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
+
+let firstName;
+firstName ="Oleh ";
+let middleName="Volodymyrovych ";
+let lastName='Maidaniuk ';
+let person=firstName+middleName+lastName;
+console.log(person);
+
+
+
+
+
+//
+// - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+//     let a = 100; let b = '100'; let c = true;
+//
+// - За допомогою 3х різних prompt() отримати 3 слова які являються вашими
+// Імям, По-Батькові та роками. та вивести в консоль
