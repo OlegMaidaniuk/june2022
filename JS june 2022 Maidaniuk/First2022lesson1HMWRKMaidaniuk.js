cbe7f582b446e3ea521c502f5e2e893b66c760e0
Olegmaidaniuk <omaydanuk@gmail.com>
2022-07-23 22:55:31 +0300

    js first m

--- JS june 2022 Maidaniuk/First2022lesson1HMWRKMaidaniuk.js
+++ JS june 2022 Maidaniuk/First2022lesson1HMWRKMaidaniuk.js
@@ -1,51 +1,104 @@
 // - Створити змінні. Присвоїти кожному з них значення:
-// 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+// // 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
 //     Вивести кожну змінну за допомогою: console.log , alert, document.write
 
 let myr;
-myr='hello';
+myr = 'hello';
 console.log(myr);
 // alert(myr);
 document.write(myr);
 
+let owu = 'owu';
+console.log(owu);
+alert(owu);
+document.write(owu);
 
+let com = 'com';
+console.log(com);
+alert(com);
+document.write(com);
 
-
+let ua = `ua`;
+console.log(ua);
+alert(ua);
+document.write(ua);
 
 let tab;
 tab = 1;
 console.log(tab)
-// alert(tab);
+alert(tab);
 document.write(tab);
 
+let tab1 = 10;
+console.log(tab1, typeof tab1);
+alert(tab1);
+document.write(tab1);
 
+let tab2 = -999;
+console.log(tab2, typeof tab2);
+document.write(typeof tab2);
+alert(tab2);
+
+let tabpi = 3.14;
+console.log(typeof tabpi);
+alert(tabpi);
+document.write(tabpi);
+
+let taboll = 123;
+let taboll1 = 2.7;
+let taboll2 = 16;
+console.log(typeof taboll, typeof taboll2, typeof taboll2, !!typeof taboll);
+prompt();
+document.write(taboll, taboll1, tab2, taboll2);
 
 let hi;
-hi=true;
+hi = true;
 console.log(hi);
 
-
-
-
-
+let fals = false;
+console.log(typeof fals);
+alert(typeof fals);
+document.write(fals);
 //
 // - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ.
 // З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
 
-let firstName;
-firstName ="Oleh ";
-let middleName="Volodymyrovych ";
-let lastName='Maidaniuk ';
-let person=firstName+middleName+lastName;
+
+let firstname = "Miroslav";
+let middlname = "Olegovich";
+let lastname = "Maidanuk";
+let fio = firstname + middlname + lastname;
+console.log(fio);
+
+let firstna = 'Oleh';
+let middlna = 'Volodymyrovych';
+let lastna = 'Maidaniuk;'
+let fml1 = `${firstna} ${middlna}  ${lastna}`;
+console.log(fml1);
+document.write(fml1);
+document.write(fml1, typeof fml1);
+
+
+//
+let firstName = "Oleh ";
+let middleName = "Volodymyrovych ";
+let lastName = 'Maidaniuk ';
+let person = firstName + middleName + lastName;
 console.log(person);
 
 
-
-
-
 //
-// - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+// - За допомогою оператора typeof визначити типи наступних змінних та вивести їх
+// в консоль.
 //     let a = 100; let b = '100'; let c = true;
-//
+// let a = 100;
+// console.log(typeof a);
+
+
 // - За допомогою 3х різних prompt() отримати 3 слова які являються вашими
 // Імям, По-Батькові та роками. та вивести в консоль
+
+let firstnam = 'Oleh Volodymyrovych';
+let middlnam = 'Volodymyrovych';
+let year = 2022;
+prompt(firstnam, middlnam, year);
